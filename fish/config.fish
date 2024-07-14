eeb3a566d03f3d545cc1d66e9b97cfd25a4e672d
Ilya Knyzhov <knyzhov@protei-lab.ru>
2024-07-14 21:49:19 +0300

    add base fish config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set PATH ~/.cargo/bin/ $PATH
+set PATH /opt/linphone/bin $PATH
+
+set -g fish_key_bindings fish_vi_key_bindings
+set -g EDITOR vim
+
