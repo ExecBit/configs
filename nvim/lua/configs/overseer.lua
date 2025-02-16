8d036deef3a4afb8e9866a53e4ed928f24f28d2e
Ilya Knyzhov <knyzhov@protei-lab.ru>
2025-02-16 16:26:12 +0300

    run_script added to overseer plugin

--- nvim/lua/configs/overseer.lua
+++ nvim/lua/configs/overseer.lua
@@ -1,6 +1,7 @@
 local setup = function()
     require("overseer").setup({
-        templates = { "builtin", "user.cpp_build" },
+        --templates = { "builtin", "user.cpp_build" },
+        templates = { "builtin", "user.run_script" },
     })
 end
 
