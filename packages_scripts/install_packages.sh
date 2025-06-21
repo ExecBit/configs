70022d0e6ab733415f5962d79a0888deedc47a40
John Doe <johndoe@example.com>
2025-06-21 14:09:57 +0300

    update

--- packages_scripts/install_packages.sh
+++ packages_scripts/install_packages.sh
@@ -1,7 +1,5 @@
 # В install_packages.sh заменить последнюю строку на:
-if command -v paru &>/dev/null; then
-  paru -S --noconfirm --needed $(cat packages.list)
-elif command -v pacman &>/dev/null; then
+if command -v pacman &>/dev/null; then
   sudo pacman -S --noconfirm --needed $(cat packages.list)
 else
   echo "Не найден ни один менеджер пакетов!"
